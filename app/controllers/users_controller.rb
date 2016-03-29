8b2c8fa2ca74a35206857bff3b5d2ba75208b871
App Academy Student <appacademy@Apps-Mac-mini-67.local>
2016-03-29 15:36:04 -0400

    Fix user create bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in(@user)
-      redirect_to topics_url
+      render :new
+      # sign_in(@user)]
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
